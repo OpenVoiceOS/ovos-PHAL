2923063168899195dbe861a172b3e0939fedc2d3
jarbasai <jarbasai@mailfence.com>
2022-02-05 11:59:31 +0000

    mycroft.conf

--- ovos_PHAL/__init__.py
+++ ovos_PHAL/__init__.py
@@ -2,13 +2,19 @@ from ovos_workshop import OVOSAbstractApplication
 from ovos_plugin_manager.phal import find_phal_plugins, PHALPlugin
 from ovos_utils.messagebus import get_mycroft_bus
 from ovos_utils import  wait_for_exit_signal
+from ovos_utils.configuration import read_mycroft_config
 from threading import Event
 from time import sleep
 
 
 class PHAL(OVOSAbstractApplication):
-    def __init__(self, config, bus=None):
+    def __init__(self, config=None, bus=None):
         super().__init__(skill_id="ovos.PHAL")
+        if not config:
+            try:
+                config = read_mycroft_config()["PHAL"]
+            except:
+                config = {}
         self.config = config
         self.bus = bus or get_mycroft_bus()
         self.drivers = {}
--- ovos_PHAL/__main__.py
+++ ovos_PHAL/__main__.py
@@ -1,7 +1,11 @@
 from ovos_PHAL import PHAL
 
-config = {
-    'ovos_phal_ipc2bus': {"enabled": True}
-}
-phal = PHAL(config)
-phal.start()
+
+if __name__ == "__main__":
+    # config read from mycroft.conf
+    # "PHAL": {
+    #     "ovos-PHAL-plugin-display-manager-ipc": {"enabled": true},
+    #     "ovos-PHAL-plugin-mk1": {"enabled": True}
+    # }
+    phal = PHAL()
+    phal.start()
