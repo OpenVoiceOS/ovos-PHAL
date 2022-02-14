a431fac224b7bdc1259f5fe1b1489ae995ca5f03
jarbasai <jarbasai@mailfence.com>
2022-02-14 18:59:53 +0000

    add tests

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='ovos_PHAL',
-    version='0.0.1a1',
+    version='0.0.1',
     packages=['ovos_PHAL'],
-    install_requires=["ovos_utils>=0.0.12"],
+    install_requires=["ovos_utils>=0.0.12", "ovos_workshop"],
     url='https://github.com/OpenVoiceOS/PHAL',
     license='apache-2.0',
     author='jarbasAi',
