b18bf0977a8961de4a6b0cc386b879bf6566975a
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-03-16 14:22:34 +0000

    process status (#4)
    * process status
    
    * typo
    
    * hooks
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- ovos_PHAL/__init__.py
+++ ovos_PHAL/__init__.py
@@ -4,13 +4,40 @@ from ovos_utils.messagebus import get_mycroft_bus
 from ovos_utils import wait_for_exit_signal
 from ovos_utils.log import LOG
 from ovos_utils.configuration import read_mycroft_config
-from threading import Event
-from time import sleep
+from ovos_utils.process_utils import ProcessStatus, StatusCallbackMap
+
+
+def on_ready():
+    LOG.info('PHAL is ready.')
+
+
+def on_stopping():
+    LOG.info('PHAL is shutting down...')
+
+
+def on_error(e='Unknown'):
+    LOG.error(f'PHAL failed to launch ({e}).')
 
 
 class PHAL(OVOSAbstractApplication):
-    def __init__(self, config=None, bus=None):
+    """
+    Args:
+        config (dict): PHAL config, usually from mycroft.conf
+        bus (MessageBusClient): mycroft messagebus connection
+        watchdog: (callable) function to call periodically indicating
+                  operational status.
+    """
+
+    def __init__(self, config=None, bus=None,
+                 on_ready=on_ready, on_error=on_error,
+                 on_stopping=on_stopping, watchdog=lambda: None):
         super().__init__(skill_id="ovos.PHAL")
+
+        callbacks = StatusCallbackMap(on_ready=on_ready,
+                                      on_error=on_error,
+                                      on_stopping=on_stopping)
+        self.status = ProcessStatus('PHAL', callback_map=callbacks)
+        self._watchdog = watchdog # TODO implement
         if not config:
             try:
                 config = read_mycroft_config()["PHAL"]
@@ -19,6 +46,7 @@ class PHAL(OVOSAbstractApplication):
         self.config = config
         self.bus = bus or get_mycroft_bus()
         self.drivers = {}
+        self.status.bind(self.bus)
 
     def load_plugins(self):
         for name, plug in find_phal_plugins().items():
@@ -36,6 +64,12 @@ class PHAL(OVOSAbstractApplication):
                     continue
 
     def start(self):
-        self.load_plugins()
-        wait_for_exit_signal()
+        self.status.set_started()
+        try:
+            self.load_plugins()
+            self.status.set_ready()
+            wait_for_exit_signal()
+        except Exception as e:
+            self.status.set_error(e)
+        self.status.set_stopping()
 
--- ovos_PHAL/__main__.py
+++ ovos_PHAL/__main__.py
@@ -1,13 +1,13 @@
-from ovos_PHAL import PHAL
+from ovos_PHAL import PHAL, on_ready, on_error, on_stopping
 
 
-def main():
+def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping):
     # config read from mycroft.conf
     # "PHAL": {
     #     "ovos-PHAL-plugin-display-manager-ipc": {"enabled": true},
     #     "ovos-PHAL-plugin-mk1": {"enabled": True}
     # }
-    phal = PHAL()
+    phal = PHAL(on_error=error_hook, on_ready=ready_hook, on_stopping=stopping_hook)
     phal.start()
     
     
