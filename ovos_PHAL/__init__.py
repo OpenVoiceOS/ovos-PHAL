d8a7f022f6b1354615211339f862d3796e00a55a
jarbasai <jarbasai@mailfence.com>
2022-11-01 20:40:13 +0000

    feat/admin_service

--- ovos_PHAL/__init__.py
+++ ovos_PHAL/__init__.py
@@ -1,9 +1,9 @@
-from ovos_workshop import OVOSAbstractApplication
-from ovos_plugin_manager.phal import find_phal_plugins, PHALPlugin
-from ovos_utils.messagebus import get_mycroft_bus
-from ovos_utils.log import LOG
+from ovos_plugin_manager.phal import find_phal_plugins, find_admin_plugins
 from ovos_utils.configuration import read_mycroft_config
+from ovos_utils.log import LOG
+from ovos_utils.messagebus import get_mycroft_bus
 from ovos_utils.process_utils import ProcessStatus, StatusCallbackMap
+from ovos_workshop import OVOSAbstractApplication
 
 
 def on_ready():
@@ -34,10 +34,12 @@ class PHAL(OVOSAbstractApplication):
         watchdog: (callable) function to call periodically indicating
                   operational status.
     """
+
     def __init__(self, config=None, bus=None,
                  on_ready=on_ready, on_error=on_error,
-                 on_stopping=on_stopping, watchdog=lambda: None, **kwargs):
-        super().__init__(skill_id="ovos.PHAL")
+                 on_stopping=on_stopping, on_started=on_started, on_alive=on_alive,
+                 watchdog=lambda: None, name="PHAL", **kwargs):
+        super().__init__(skill_id=f"ovos.{name}")
         ready_hook = kwargs.get('ready_hook', on_ready)
         error_hook = kwargs.get('error_hook', on_error)
         stopping_hook = kwargs.get('stopping_hook', on_stopping)
@@ -48,8 +50,8 @@ class PHAL(OVOSAbstractApplication):
                                       on_stopping=stopping_hook,
                                       on_alive=alive_hook,
                                       on_started=started_hook)
-        self.status = ProcessStatus('PHAL', callback_map=callbacks)
-        self._watchdog = watchdog # TODO implement
+        self.status = ProcessStatus(name, callback_map=callbacks)
+        self._watchdog = watchdog  # TODO implement
         if not config:
             try:
                 config = read_mycroft_config()["PHAL"]
@@ -86,3 +88,60 @@ class PHAL(OVOSAbstractApplication):
     def shutdown(self):
         self.status.set_stopping()
 
+
+def on_admin_ready():
+    LOG.info('PHAL Admin is ready.')
+
+
+def on_admin_stopping():
+    LOG.info('PHAL Admin is shutting down...')
+
+
+def on_admin_error(e='Unknown'):
+    LOG.error(f'PHAL Admin failed to launch ({e}).')
+
+
+def on_admin_alive():
+    LOG.info('PHAL Admin is alive')
+
+
+def on_admin_started():
+    LOG.info('PHAL Admin is started')
+
+
+class AdminPHAL(OVOSAbstractApplication):
+    """
+    Args:
+        config (dict): PHAL admin config, usually from mycroft.conf PHAL.admin section
+        bus (MessageBusClient): mycroft messagebus connection
+        watchdog: (callable) function to call periodically indicating
+                  operational status.
+    """
+
+    def __init__(self, config=None, bus=None, on_ready=on_admin_ready, on_error=on_admin_error,
+                 on_stopping=on_admin_stopping, on_started=on_admin_started, on_alive=on_admin_alive,
+                 watchdog=lambda: None, name="PHAL.admin", **kwargs):
+        if not config:
+            try:
+                config = read_mycroft_config()
+                config = config.get("PHAL", {}).get("admin", {})
+            except:
+                config = {}
+        super().__init__(config, bus, on_ready, on_error, on_stopping, on_started, on_alive, watchdog, name, **kwargs)
+
+    def load_plugins(self):
+        for name, plug in find_admin_plugins().items():
+            config = self.config.get(name) or {}
+            enabled = config.get("enabled")
+            if not enabled:
+                continue  # require explicit enabling by user
+            if hasattr(plug, "validator"):
+                enabled = plug.validator.validate(config)
+
+            if enabled:
+                try:
+                    self.drivers[name] = plug(bus=self.bus, config=config)
+                    LOG.info(f"PHAL Admin plugin loaded: {name}")
+                except Exception:
+                    LOG.exception(f"failed to load PHAL Admin plugin: {name}")
+                    continue
